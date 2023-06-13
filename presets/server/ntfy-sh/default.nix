df07fe6adec6de3d9166023b508c94b0a1f39244
emilylange <git@emilylange.de>
2023-06-13 17:57:33 +0200

    feat(ntfy-sh): enable email notifications

--- presets/server/ntfy-sh/default.nix
+++ presets/server/ntfy-sh/default.nix
@@ -1,6 +1,6 @@
 ## ntfy user add --role=admin <username>
 ## ntfy access '*' 'up*' write-only
-{ ... }:
+{ config, ... }:
 
 {
   services.ntfy-sh = {
@@ -18,8 +18,26 @@
       cache-duration = "72h";
 
       enable-signup = false;
+      visitor-email-limit-burst = 0;
     };
   };
 
-  systemd.services.ntfy-sh.serviceConfig.RuntimeDirectory = [ "ntfy-sh" ];
+  systemd.services.ntfy-sh = {
+    serviceConfig = {
+      RuntimeDirectory = [ "ntfy-sh" ];
+      EnvironmentFile = config.deployment.keys."ntfy_additional_env".path;
+    };
+    unitConfig.ConditionPathExists = [ config.deployment.keys."ntfy_additional_env".path ];
+  };
+
+  deployment.keys."ntfy_additional_env" = {
+    destDir = "/";
+    ## ```env
+    ## NTFY_SMTP_SENDER_ADDR=
+    ## NTFY_SMTP_SENDER_USER=
+    ## NTFY_SMTP_SENDER_PASS=
+    ## NTFY_SMTP_SENDER_FROM=
+    ## ```
+    keyCommand = [ "bw" "--nointeraction" "get" "notes" "gkcl/ntfy_additional_env" ];
+  };
 }
