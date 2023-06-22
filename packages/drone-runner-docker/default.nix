7f7121a5cd5cc75c3c6d7809f9ad70a52e5cd667
emilylange <git@emilylange.de>
2023-06-22 23:35:09 +0200

    fix(drone-runner-docker): pin patch via commit sha

--- packages/drone-runner-docker/default.nix
+++ packages/drone-runner-docker/default.nix
@@ -5,8 +5,9 @@ pkgs.drone-runner-docker.overrideAttrs (finalAttrs: previousAttrs: {
     (pkgs.fetchpatch {
       ## PR: "Add option to enable IPv6 support in docker networks"
       ## https://github.com/drone-runners/drone-runner-docker/pull/58
-      url = "https://patch-diff.githubusercontent.com/raw/drone-runners/drone-runner-docker/pull/58.patch";
-      sha256 = "sha256-sIi1PewmGf7kRU+6blfShCZRkxAp8RWp6YP/P9nbC1Q=";
+      url = "https://github.com/drone-runners/drone-runner-docker/commit/53a5e2344779878295e9a4b4f79cd96414c06e2a.patch";
+      hash = "sha256-sIi1PewmGf7kRU+6blfShCZRkxAp8RWp6YP/P9nbC1Q=";
+      name = "58.patch";
     })
   ];
 })
