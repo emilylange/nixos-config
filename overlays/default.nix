5caae4a2486805bd659e1339e7bc0930f9c99cad
emilylange <git@emilylange.de>
2023-09-26 18:32:16 +0200

    feat(matrix-synapse): don't change displayname in all rooms for `PUT /displayname`
    https://spec.matrix.org/v1.8/client-server-api/#put_matrixclientv3profileuseriddisplayname

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,1 +1,12 @@
-[ ]
+[
+  (final: prev: {
+    matrix-synapse-unwrapped = prev.matrix-synapse-unwrapped.overrideAttrs (finalAttrs: previousAttrs: {
+      patches = (previousAttrs.patches or [ ]) ++ [
+        ./matrix-synapse-unwrapped/dont-change-roomstate-on-displayname-change.patch
+      ];
+
+      doCheck = false;
+      doInstallCheck = false;
+    });
+  })
+]
