4b2cad46405a576eda35c0a937e27f8f17c27bde
emilylange <git@emilylange.de>
2023-10-10 16:17:44 +0200

    feat(wayland): enable xdg-portal

--- presets/desktop/defaults/wayland.nix
+++ presets/desktop/defaults/wayland.nix
@@ -1,6 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   hardware.opengl.enable = true;
   security.polkit.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+  };
 }
