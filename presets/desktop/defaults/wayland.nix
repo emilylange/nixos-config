5573bf64594b0c17784e4e9a0c6db39eb6ed561a
emilylange <git@emilylange.de>
2023-11-13 00:09:48 +0100

    feat(desktop): enable auto-log into sway

--- presets/desktop/defaults/wayland.nix
+++ presets/desktop/defaults/wayland.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   hardware.opengl.enable = true;
@@ -11,4 +11,19 @@
       xdg-desktop-portal-gtk
     ];
   };
+
+  ## auto-login for user "me" on tty1.
+  ## assumes sway to be enabled and configured in home-manager.
+  services.greetd = {
+    enable = true;
+    restart = false;
+    settings = {
+      initial_session = {
+        command = "sway";
+        user = "me";
+      };
+      default_session.command = "${config.services.greetd.package}/bin/agreety --cmd sway";
+    };
+  };
+
 }
