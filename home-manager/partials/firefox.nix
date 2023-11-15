f2104b3d2329d2f564ac239f4c909968e86213a1
emilylange <git@emilylange.de>
2023-11-15 14:42:14 +0100

    feat(firefox): disable firefox relay prompt in forms

--- home-manager/partials/firefox.nix
+++ home-manager/partials/firefox.nix
@@ -11,6 +11,9 @@
           "browser.startup.homepage" = "about:blank";
           "browser.toolbars.bookmarks.visibility" = "never";
           "network.protocol-handler.external.mailto" = false; ## hide 'Add "$x" as an application for mailto links?' bar
+          "signon.firefoxRelay.feature" = "disabled";
+
+          ## DNS over HTTPS (DoH)
           "network.trr.mode" = 2;
           "network.trr.uri" = "https://open.dns0.eu";
         } // lib.optionalAttrs (osConfig.networking.hostName == "ryzen") {
