c1718d06c9cdfbb9bfd7fd19fe5c9781eb10d900
emilylange <git@emilylange.de>
2024-01-21 23:59:46 +0100

    fix(miniflux): fix eval error due to newly introduced value conflict
    The conflict was introduced due to the module option
    `services.miniflux.config` now also allowing the type int besides
    string.
    
    Along with this change, the module also change the default types (for
    config options like `RUN_MIGRATIONS` and `CREATE_ADMIN` from `"1"`
    (string) to `1` (int).
    
    This commit changes the explicitly defined (but same as upstream) values
    from string to int, where possible, to prevent collisions, so eval
    succeeds again after the nixpkgs flake bump.

--- presets/server/miniflux/default.nix
+++ presets/server/miniflux/default.nix
@@ -5,16 +5,16 @@
     enable = true;
     config = {
       BASE_URL = "https://rss.geklaute.cloud/";
-      CLEANUP_ARCHIVE_READ_DAYS = "-1";
-      CLEANUP_ARCHIVE_UNREAD_DAYS = "-1";
-      CREATE_ADMIN = lib.mkForce "0";
-      HTTP_CLIENT_TIMEOUT = "120";
-      HTTPS = "1";
+      CLEANUP_ARCHIVE_READ_DAYS = -1;
+      CLEANUP_ARCHIVE_UNREAD_DAYS = -1;
+      CREATE_ADMIN = lib.mkForce 0;
+      HTTP_CLIENT_TIMEOUT = 120;
+      HTTPS = 1;
       LISTEN_ADDR = "/run/miniflux/miniflux.sock";
-      POLLING_FREQUENCY = "20";
-      POLLING_PARSING_ERROR_LIMIT = "-1";
+      POLLING_FREQUENCY = 20;
+      POLLING_PARSING_ERROR_LIMIT = -1;
       PROXY_IMAGES = "all";
-      RUN_MIGRATIONS = "1";
+      RUN_MIGRATIONS = 1;
     };
     adminCredentialsFile = pkgs.emptyFile;
   };
