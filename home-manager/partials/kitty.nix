d15e6b5133d469712b555c8e256f9bb8a125ad8b
emilylange <git@emilylange.de>
2024-02-10 20:02:01 +0100

    fix(kitty): fix `shell_integration` setting after home-manager bump

--- home-manager/partials/kitty.nix
+++ home-manager/partials/kitty.nix
@@ -6,9 +6,11 @@
     settings = {
       enable_audio_bell = false;
       update_check_interval = 0; ## disable periodic (24h) version update check
-      shell_integration = "no-cursor no-prompt-mark"; ## disable "click to move cursor" functionality but keep cursor block-y
       scrollback_lines = 10000; ## increase from default 2000 lines
       scrollback_pager_history_size = 50; ## [in MB], only when using pager
     };
+
+    # this used to be in the freeform programs.kitty.settings option, until https://github.com/nix-community/home-manager/pull/4629
+    shellIntegration.mode = "no-cursor no-prompt-mark"; ## disable "click to move cursor" functionality but keep cursor block-y
   };
 }
