68fcfd64ba5e141b223967c37af6508126073fe0
emilylange <git@emilylange.de>
2024-04-19 19:02:09 +0200

    feat(firefox): disable safebrowsing in profile
    and remove no longer needed frame_rate override

--- home-manager/partials/firefox.nix
+++ home-manager/partials/firefox.nix
@@ -1,4 +1,4 @@
-{ lib, osConfig, ... }:
+{ ... }:
 
 {
   programs.firefox = {
@@ -13,13 +13,14 @@
           "network.protocol-handler.external.mailto" = false; ## hide 'Add "$x" as an application for mailto links?' bar
           "signon.firefoxRelay.feature" = "disabled";
 
+          "browser.safebrowsing.downloads.enabled" = false;
+          "browser.safebrowsing.malware.enabled" = false;
+          "browser.safebrowsing.phishing.enabled" = false;
+
           ## DNS over HTTPS (DoH)
           ## https://wiki.mozilla.org/Trusted_Recursive_Resolver
           "network.trr.mode" = 3;
           "network.trr.uri" = "https://dns.mullvad.net/dns-query";
-        } // lib.optionalAttrs (osConfig.networking.hostName == "ryzen") {
-          ## enable high refresh rate for desktop
-          "layout.frame_rate" = 144;
         };
       };
     };
